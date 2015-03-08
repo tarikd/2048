cf50df22cb0835176680295820a09209fb51c9e4
Tarik Dadi <tarikdadi@me.com>
2015-03-08 17:09:22 +0100

    fix

--- playAI.py
+++ playAI.py
@@ -4,14 +4,14 @@ import solver
 
 b = solver.SimpleAI(.25)
 
-b.goal = 2048
+b.goal = 1024
 #b.prob = 1
 
 
 
 
 # Je fais tourner mon AI naïve sur X board à la suite, jusqu'à ce que j'atteigne le GOAL fixé
-b.simPlay(100)
+b.simPlay(10)
 
 
 # Je fais tourner mon AI naïve sur un seul board, jusqu'à ce qu'il y ai un Game Over.
